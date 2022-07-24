e326808f5a9ec1554372d3630bd4278f1b1fd3e4
jfrausto7 <jacob_frausto@brown.edu>
2022-07-24 16:15:28 -0700

    add main method for taking in and displaying data

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import argparse
 
 import torch
+from torch.utils.data import DataLoader
+
+from utils.dataset import FreiHAND
+from utils.utils import show_data
 
 # TODO: adjust following config vals
 config = {
@@ -49,3 +53,33 @@ def parse_args() -> argparse.Namespace:
     )
 
     return parser.parse_args()
+
+
+def get_split_data():
+    # Training dataset split
+    train_dataset = FreiHAND(config=config, set_type="train")
+    train_dataloader = DataLoader(
+        train_dataset, config["batch_size"], shuffle=True, drop_last=True, num_workers=2
+    )
+
+    # Validation dataset split
+    val_dataset = FreiHAND(config=config, set_type="val")
+    print(val_dataset.__len__())
+    val_dataloader = DataLoader(
+        val_dataset, config["batch_size"], shuffle=True, drop_last=True, num_workers=2
+    )
+
+    # Visualize a random batch of data train samples & labels
+    show_data(train_dataset)
+
+
+def main(args: argparse.Namespace) -> None:
+    print("hello")
+    if args.train:
+        print("getting data")
+        get_split_data()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,5 @@
 from cv2 import GaussianBlur
+from matplotlib import pyplot as plt
 import numpy as np
 
 
@@ -8,6 +9,13 @@ RAW_IMG_SIZE = 224
 MODEL_IMG_SIZE = 128
 DATASET_MEANS = [0.3950, 0.4323, 0.2954]
 DATASET_STDS = [0.1966, 0.1734, 0.1836]
+COLORMAP = {
+    "thumb": {"ids": [0, 1, 2, 3, 4], "color": "g"},
+    "index": {"ids": [0, 5, 6, 7, 8], "color": "c"},
+    "middle": {"ids": [0, 9, 10, 11, 12], "color": "b"},
+    "ring": {"ids": [0, 13, 14, 15, 16], "color": "m"},
+    "little": {"ids": [0, 17, 18, 19, 20], "color": "r"},
+}
 
 
 def projectPoints(xyz, K):
@@ -45,3 +53,33 @@ def vector_to_heatmaps(keypoints):
 
     heatmaps = blur_heatmaps(heatmaps)
     return heatmaps
+
+def show_data(dataset, n_samples=12):
+    """
+    Function to visualize data
+    Input: torch.utils.data.Dataset
+    """
+    n_cols = 4
+    n_rows = int(np.ceil(n_samples / n_cols))
+    plt.figure(figsize=[15, n_rows * 4])
+
+    ids = np.random.choice(dataset.__len__(), n_samples, replace=False)
+    for i, id_ in enumerate(ids, 1):
+        sample = dataset.__getitem__(id_)
+
+        image = sample["image_raw"].numpy()
+        image = np.moveaxis(image, 0, -1)
+        keypoints = sample["keypoints"].numpy()
+        keypoints = keypoints * RAW_IMG_SIZE
+
+        plt.subplot(n_rows, n_cols, i)
+        plt.imshow(image)
+        plt.scatter(keypoints[:, 0], keypoints[:, 1], c="k", alpha=0.5)
+        for finger, params in COLORMAP.items():
+            plt.plot(
+                keypoints[params["ids"], 0],
+                keypoints[params["ids"], 1],
+                params["color"],
+            )
+    plt.tight_layout()
+    plt.show()
