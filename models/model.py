3c1540b2ba657bc8fd7d3bd1cf7e43b3d03c7df0
jfrausto7 <jacob_frausto@brown.edu>
2022-07-24 16:58:25 -0700

    Update model.py

--- models/model.py
+++ models/model.py
@@ -2,5 +2,23 @@ from torch import nn
 
 
 class ViT(nn.Sequential):
-    def __init__(self):
+    """
+    Implementation of Vision Transformer (ViT) proposed in 
+    `An Image Is Worth 16x16 Words: Transformers For Image Recognition At Scale`.
+
+    This model architecture is repurposed/redesigned here for the purpose of 2D
+    Hand Pose Estimation.
+    
+    Link to original paper: https://arxiv.org/pdf/2010.11929.pdf_
+    """
+
+    def __init__(
+        self,
+        in_channels: int = 3,
+        patch_size: int = 16,
+        emb_size: int = 768,
+        img_size: int = 224,
+        depth: int = 12,
+        out_channels: int = 50 ** kwargs,
+    ):
         super().__init__()
